9f3d82cc83f359a079a4d521098f252406ed7d8a
VegK <vegkgh@gmail.com>
2017-08-26 19:09:42 +0300

    Unity project

---
+++ Assets/Scripts/TransferData.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TransferData : MonoBehaviour
+{
+    [SerializeField]
+    private Text contentPlayerPrefsText;
+    [SerializeField]
+    private Text contentLocalStorageText;
+
+    public void ClickSetPlayerPrefsHandler()
+    {
+        PlayerPrefs.SetString("command", "This is test work PlayerPrefs. Time " + Time.time);
+    }
+
+    public void ClickGetPlayerPrefsHandler()
+    {
+        contentPlayerPrefsText.text = PlayerPrefs.GetString("command", "Not work. Time " + Time.time);
+    }
+
+    public void ClickSetLocalStorageHandler()
+    {
+        Application.ExternalCall("setLocalStorage", "This is test work ExternalCall. Time " + Time.time);
+    }
+
+    public void GetLocalStorage(string message)
+    {
+        contentLocalStorageText.text = message;
+    }
+
+    public void ClickOpenWindow()
+    {
+        Application.ExternalCall("openWindow");
+    }
+}
